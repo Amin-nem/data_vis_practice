744ef464be5fb4711324380f722c00288c79f27d
Amin <neemamin22@gmail.com>
2024-02-16 12:29:33 +0330

    plotting a series of points

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+# style
 plt.style.use('fivethirtyeight')
+# data
+x_values = range(-50,50)
+y_values = [i**2 for i in x_values]
+#plot
 fig,ax = plt.subplots()
-ax.scatter(2,4,s=200)
-
+ax.scatter(x_values,y_values,s=30)
 # chat title and label
 ax.set_title('Square Numbers')
 ax.set_xlabel('value')
