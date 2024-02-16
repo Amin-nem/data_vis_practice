9ee5223ad083b59130078240c117b247d72f066e
Amin <neemamin22@gmail.com>
2024-02-16 08:00:50 +0330

    using built-in styles

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,14 +2,32 @@ import matplotlib.pyplot as plt
 nums = range(-100,101)
 
 squares = [i**2 for i in range(-100,101)]
+
+#style
+plt.style.use('seaborn-v0_8-pastel')
+
 fig, ax = plt.subplots()
-ax.plot(nums,squares,linewidth=3)
+ax.plot(
+    nums,
+    squares,
+    #linewidth=3
+    )
 
 # title and axes' label
-ax.set_title('Square Numbers',fontsize=24,color='red')
-ax.set_xlabel('Value',fontsize=15)
-ax.set_ylabel('Square of Value',fontsize=15)
+ax.set_title(
+    'Square Numbers',
+    #fontsize=24,
+    #color='red'
+    )
+ax.set_xlabel('Value',
+    #fontsize=15,
+)
+ax.set_ylabel(
+    'Square of Value',
+    #fontsize=15,
+)
 
 # size of tick labels
 ax.tick_params(labelsize=10)
+print(plt.style.available)
 plt.show()
