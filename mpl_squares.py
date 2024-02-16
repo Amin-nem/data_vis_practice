10daf10733308f88cb6307fe1086c5f6b1198b4f
Amin <neemamin22@gmail.com>
2024-02-16 07:15:02 +0330

    correcting the plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
-squares = [i**2 for i in range(-2500,2501)]
+nums = range(-100,101)
+
+squares = [i**2 for i in range(-100,101)]
 fig, ax = plt.subplots()
-ax.plot(squares,linewidth=3)
+ax.plot(nums,squares,linewidth=3)
 
 # title and axes' label
 ax.set_title('Square Numbers',fontsize=24,color='red')
-ax.set_xlabel('Value',fontsize=24)
-ax.set_ylabel('Square of Value',fontsize=24)
+ax.set_xlabel('Value',fontsize=15)
+ax.set_ylabel('Square of Value',fontsize=15)
 
 # size of tick labels
-ax.tick_params(labelsize=14)
+ax.tick_params(labelsize=10)
 plt.show()
