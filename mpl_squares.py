1efe208d43b5525d2a036d2c98c0ab5650d89766
Amin <neemamin22@gmail.com>
2024-02-16 06:33:46 +0330

    changing the labels and line thikness

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
-squares = [i**2 for i in range(25000)]
+squares = [i**2 for i in range(-2500,2501)]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares,linewidth=3)
+
+# title and axes' label
+ax.set_title('Square Numbers',fontsize=24,color='red')
+ax.set_xlabel('Value',fontsize=24)
+ax.set_ylabel('Square of Value',fontsize=24)
+
+# size of tick labels
+ax.tick_params(labelsize=14)
 plt.show()
