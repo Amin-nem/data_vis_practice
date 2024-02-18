fd8b8eff90f218344b7dd9444e1d332909b0b539
Amin <neemamin22@gmail.com>
2024-02-18 18:29:06 +0330

    Using a colormap

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,7 +6,7 @@ x_values = range(-50,51)
 y_values = [i**2 for i in x_values]
 #plot
 fig,ax = plt.subplots()
-ax.scatter(x_values,y_values,s=10,color=(0,0.8,0))
+ax.scatter(x_values,y_values,s=10,c=y_values,cmap=plt.cm.Blues)
 # chat title and label
 ax.set_title('Square Numbers')
 ax.set_xlabel('value')
