d8db3c7dd5cf9177a11c55680a31e6362b74392c
Amin <neemamin22@gmail.com>
2024-02-20 19:02:14 +0330

    altering the size

--- random_walk_visualize.py
+++ random_walk_visualize.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 # let's make a random walk
-
-rw = RandomWalk()
+rw = RandomWalk(100000)
 rw.fill_walk()
 
 # Plot the points in the walk.
 plt.style.use('classic')
-fix, ax = plt.subplots()
+fix, ax = plt.subplots(figsize=(15,9))
 point_numbers = range(rw.num_points)
-ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues, edgecolors='none',s=15)
+ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues, edgecolors='none',s=1)
 ax.set_aspect('equal')
 
 # Emphasize the first and the last points
