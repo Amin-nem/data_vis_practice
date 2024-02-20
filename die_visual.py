b3b05804f69b3637f075429cdfe49a26254e3d91
Amin <neemamin22@gmail.com>
2024-02-20 19:14:32 +0330

    Analyze the results

---
+++ die_visual.py
@@ -0,0 +1,15 @@
+from die import Die
+# Create a D6
+
+die = Die()
+# roll and store the resutls
+results =[die.roll() for i in range(1000)]
+
+# Analyze the results
+frequencies = []
+poss_results = range(1, die.num_sides+1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
